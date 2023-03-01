57ea297eb818b226300c70fee371b7cdf62986e2
Tico Esteves <ticoestevescontato@gmail.com>
2023-03-01 14:55:23 -0300

    alteração arquivo js + animações

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,15 @@
+var menu = document.getElementById("menu_mobile");
 function MostraMenuMobile() {
-    var menu = document.getElementById('menu_mobile');
 
-    if(menu.style.display == 'none') {
-        menu.style.display = 'block';
+    if(menu.style.display == "none") {
+        menu.style.display = "block";
+        menu.classList.add("animate__fadeInDown");
     } else {
-        menu.style.display = 'none';
+        menu.style.display = "none";
+        menu.classList.remove("animate__fadeInDown");
     }
+}
+
+function hideMenu() {
+    menu.style.display = "none";
 }
